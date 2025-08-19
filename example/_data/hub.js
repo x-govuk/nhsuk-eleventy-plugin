6c74d8142662bb6268856a18ec7e3f57333d92a5
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-08-19 22:40:59 +0100

    Update data in example site to provide a title for each item

--- example/_data/hub.js
+++ example/_data/hub.js
@@ -2,7 +2,7 @@ export default {
   section1: [
     {
       href: '#',
-      text: '5 steps to mental wellbeing',
+      title: '5 steps to mental wellbeing',
       date: '2022-12-16',
       description:
         'Practical advice to help you feel mentally and emotionally better',
@@ -12,7 +12,7 @@ export default {
     },
     {
       href: '#',
-      text: '10 stress busters',
+      title: '10 stress busters',
       date: '2022-12-16',
       description:
         'If you’re stressed, whether by your job or something more personal, the first step to feeling better is to identify the cause.',
@@ -22,7 +22,7 @@ export default {
     },
     {
       href: '#',
-      text: 'Exercise for depression',
+      title: 'Exercise for depression',
       date: '2022-12-16',
       description:
         'Being depressed can leave you feeling low in energy, which might put you off being more active.',
@@ -34,48 +34,48 @@ export default {
   section2: [
     {
       href: '#',
-      text: 'Healthy weight',
+      title: 'Healthy weight',
       description:
         'Information for adults and children about healthy weight, including advice to help you lose weight'
     },
     {
       href: '#',
-      text: 'Exercise',
+      title: 'Exercise',
       description:
         'Exercise guidelines and workouts to help improve your fitness and wellbeing.'
     },
     {
       href: '#',
-      text: 'Sleep and tiredness',
+      title: 'Sleep and tiredness',
       description:
         'Reasons why you might feel tired and advice about what you can do to prevent tiredness.'
     },
     {
       href: '#',
-      text: 'Eat well',
+      title: 'Eat well',
       description:
         'Information and guidance about eating a healthy, balanced diet.'
     },
     {
       href: '#',
-      text: 'Alcohol advice',
+      title: 'Alcohol advice',
       description:
         'Advice and support for reducing the amount of alcohol you drink.'
     },
     {
       href: '#',
-      text: 'Quit smoking',
+      title: 'Quit smoking',
       description: 'Information and advice to help you stop smoking.'
     },
     {
       href: '#',
-      text: 'Addiction support',
+      title: 'Addiction support',
       description:
         'Help and support for addiction, including drug and gambling addictions.'
     },
     {
       href: '#',
-      text: 'Sexual health',
+      title: 'Sexual health',
       description:
         'Information and advice about sexual health, including contraception and sexually transmitted infections (STIs).'
     }
@@ -83,34 +83,34 @@ export default {
   section3: [
     {
       href: '#',
-      text: 'Bone health',
+      title: 'Bone health',
       description: 'Advice about keeping your bones healthy.'
     },
     {
       href: '#',
-      text: 'Pain',
+      title: 'Pain',
       description: 'Support and advice to help with your pain.'
     },
     {
       href: '#',
-      text: 'Healthy teeth and gums',
+      title: 'Healthy teeth and gums',
       description:
         'Information and advice about looking after your teeth and gums.'
     },
     {
       href: '#',
-      text: 'Seasonal health',
+      title: 'Seasonal health',
       description:
         'Information and advice on staying well during summer and winter.'
     },
     {
       href: '#',
-      text: 'How to wash your hands',
+      title: 'How to wash your hands',
       description: 'Step-by-step guide to washing your hands.'
     },
     {
       href: '#',
-      text: 'When your child is too ill for school',
+      title: 'When your child is too ill for school',
       description: 'Advice about when to keep your child off school or nursery.'
     }
   ]
--- example/_data/posts.js
+++ example/_data/posts.js
@@ -1,35 +1,35 @@
 export default [
   {
     href: '#',
-    text: 'Why should we build services using a design system?',
+    title: 'Why should we build services using a design system?',
     date: '2022-06-08',
     description:
       'Conor Rohan, Assurance Lead, Department of Health and Social Care, explains why we use the NHS.UK and GOV.UK design systems and which one to use.'
   },
   {
     href: '#',
-    text: '(Re)using the right tools for the job',
+    title: '(Re)using the right tools for the job',
     date: '2022-05-07',
     description:
       'Shan Rahulan, Director of Platforms – Core Services, argues that we should not try to reinvent the wheel every time we build a product.'
   },
   {
     href: '#',
-    text: 'How a 20-year-old standard is still relevant today',
+    title: 'How a 20-year-old standard is still relevant today',
     date: '2022-03-08',
     description:
       'Tero Väänänen says BS EN ISO 9241-210 remains a guiding star for human-centred design. It’s the foundation to our own NHS service standard and the GOV.UK service standard.'
   },
   {
     href: '#',
-    text: 'Encouraging a community to contribute to a design system',
+    title: 'Encouraging a community to contribute to a design system',
     date: '2021-05-21',
     description:
       'Co-authored by Henry Cookson (user research) and Tosin Balogun (interaction design), user centred design (UCD) trainees who worked on the service manual team.'
   },
   {
     href: '#',
-    text: '10 ways the NHS digital service manual can help you',
+    title: '10 ways the NHS digital service manual can help you',
     date: '2020-11-12',
     description:
       'If you want to build easy-to-use digital health services that put people first, Adrian Smith explains why the NHS digital service manual is a great place to start.'
